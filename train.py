c00c66b79c5a75e4a5d9c34b35cb7c7fbc670bb6
Jelle Bouwman <32744870+jellebouwman@users.noreply.github.com>
2022-07-12 09:01:37 +0700

    Add requirements for MLEM, run 'mlem init'

---
+++ train.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+from mlem.api import save
+
+
+def main():
+    data, y = load_iris(return_X_y=True, as_frame=True)
+    rf = RandomForestClassifier(
+        n_jobs=2,
+        random_state=42,
+    )
+    rf.fit(data, y)
+
+    save(
+        rf,
+        "rf",
+        sample_data=data,
+        description="Random Forest Classifier",
+    )
+
+
+if __name__ == "__main__":
+    main()
